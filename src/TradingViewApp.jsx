889bf1fd4af93f1145a58146cf64d06c11346700
Gary McNeish <101473124+Gary-ForestStaking@users.noreply.github.com>
2023-09-25 00:25:14 +0700

    Update TradingViewApp.jsx

--- src/TradingViewApp.jsx
+++ src/TradingViewApp.jsx
@@ -1,49 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 export default function TradingViewApp() {
+  const onLoadScriptRef = useRef();
 
   useEffect(() => {
+    onLoadScriptRef.current = createWidget;
+
     const script = document.createElement('script');
     script.id = 'tradingview-widget-loading-script';
     script.src = 'https://s3.tradingview.com/tv.js';
     script.type = 'text/javascript';
-    script.onload = createWidgets;
+    script.onload = () => onLoadScriptRef.current && onLoadScriptRef.current();
 
     document.head.appendChild(script);
 
-    function createWidgets() {
-      if ('TradingView' in window) {
-        // First widget
-        new window.TradingView.widget({
-          container_id: "tradingview_2b44c_1",
-          autosize: true,
-          symbol: "BINANCE:BTCUSDT",
-          interval: "1",
-          timezone: "Etc/UTC",
-          theme: "dark",
-          style: "1",
-          enable_publishing: false,
-          backgroundColor: "rgba(0, 0, 0, 1)",
-          gridColor: "rgba(0, 0, 0, 0.06)",
-          hide_top_toolbar: true,
-          save_image: false,
-          studies: ["STD;Bollinger_Bands"]
-        });
+    return () => onLoadScriptRef.current = null;
 
-        // Second widget
+    function createWidget() {
+      if (document.getElementById('tradingview_d49c3') && 'TradingView' in window) {
         new window.TradingView.widget({
-          container_id: "tradingview_2b44c_2",
-          autosize: true,
+          container_id: "tradingview_d49c3",
+          width: 1920,
+          height: 1080,
           symbol: "BINANCE:BTCUSDT",
           interval: "1",
           timezone: "Etc/UTC",
-          theme: "dark",
+          theme: "light",
           style: "1",
           enable_publishing: false,
           backgroundColor: "rgba(0, 0, 0, 1)",
           gridColor: "rgba(0, 0, 0, 0.06)",
           hide_top_toolbar: true,
           save_image: false,
+          details: true,
           studies: ["STD;Bollinger_Bands"]
         });
       }
@@ -51,24 +40,5 @@ export default function TradingViewApp() {
   }, []);
 
   return (
-    <div style={{ backgroundColor: 'black' }}>
-      <div className="tradingview-widget-container">
-        <div id="tradingview_2b44c_1"></div>
-        <div className="tradingview-widget-copyright">
-          <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank">
-            <span className="blue-text">Track all markets on TradingView</span>
-          </a>
-        </div>
-      </div>
-
-      <div className="tradingview-widget-container">
-        <div id="tradingview_2b44c_2"></div>
-        <div className="tradingview-widget-copyright">
-          <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank">
-            <span className="blue-text">Track all markets on TradingView</span>
-          </a>
-        </div>
-      </div>
-    </div>
   );
 }
