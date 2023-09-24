dc574ebda27ae751dbab10aca65a022bd6baf404
Gary McNeish <101473124+Gary-ForestStaking@users.noreply.github.com>
2023-09-25 00:20:20 +0700

    Update TradingViewApp.jsx

--- src/TradingViewApp.jsx
+++ src/TradingViewApp.jsx
@@ -51,6 +51,24 @@ export default function TradingViewApp() {
   }, []);
 
   return (
+    <div style={{ backgroundColor: 'black' }}>
+      <div className="tradingview-widget-container">
+        <div id="tradingview_2b44c_1"></div>
+        <div className="tradingview-widget-copyright">
+          <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank">
+            <span className="blue-text">Track all markets on TradingView</span>
+          </a>
+        </div>
+      </div>
 
+      <div className="tradingview-widget-container">
+        <div id="tradingview_2b44c_2"></div>
+        <div className="tradingview-widget-copyright">
+          <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank">
+            <span className="blue-text">Track all markets on TradingView</span>
+          </a>
+        </div>
+      </div>
+    </div>
   );
 }
